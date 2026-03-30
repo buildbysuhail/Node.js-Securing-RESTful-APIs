9aa8499207eeecea51ddb37c1a8e8e4612a6657e
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-03-30 18:29:28 +0530

    Add the handlers for login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -14,36 +14,46 @@ export const loginRequired = (req, res, next) => {
 };
 
 export const register = (req, res) => {
-    const newUser = new User(req.body);
+  const newUser = new User(req.body);
 
-    newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
-    newUser.save((err, user) => {
-        if (err) {
-            return res.status(400).send({
-                message: err
-            })
-        } else {
-            user.hashPassword = undefined;
-            return res.json(user);
-        }
-    })
-}
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        message: err,
+      });
+    } else {
+      user.hashPassword = undefined;
+      return res.json(user);
+    }
+  });
+};
 
 export const login = (req, res) => {
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-       if (err) throw err;
-       if (!user) {
-            res.status(401).json({ message: "Authentication failed. User not found." });
-       } else if (user) {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res
+          .status(401)
+          .json({ message: "Authentication failed. User not found." });
+      } else if (user) {
         if (!user.comparePassword(req.body.password, user.hashPassword)) {
-            return res.status(401).json({ message: "Authentication failed. Wrong password." });
-       } else {
-            return res.json({ token: jwt.sign({ email: user.email, username:
-                user.username, _id: user.id
-             }, 'RESTFULAPIs') });
-       }
-       }
-    });
-}
+          return res
+            .status(401)
+            .json({ message: "Authentication failed. Wrong password." });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              { email: user.email, username: user.username, _id: user.id },
+              "RESTFULAPIs",
+            ),
+          });
+        }
+      }
+    },
+  );
+};
