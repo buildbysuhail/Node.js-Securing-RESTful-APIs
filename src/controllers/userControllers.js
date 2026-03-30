063c8bde6daaf6b303182f083062d3dd47276cb7
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-03-30 17:09:23 +0530

    Securing the Node API: Add the handlers for registration

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -7,8 +7,24 @@ const User = mongoose.model("User", userSchema);
 
 export const loginRequired = (req, res, next) => {
   if (req.user) {
-    next();
+    next(); // If User is there then go to next function
   } else {
     return res.status(401).json({ message: "Unauthorized user!" });
   }
 };
+
+export const register = (req, res) => {
+    const newUser = new User(req.body);
+
+    newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).send({
+                message: err
+            })
+        } else {
+            user.hashPassword = undefined;
+            return res.json(user);
+        }
+    })
+}
